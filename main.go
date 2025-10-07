0e22bbd4a8034297b0964246945c3cd091df3f47
Andrea Swearingen <swearing.ais@gmail.com>
2025-10-06 23:10:07 -0700

    ensure prng returns a number

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
 // This will be the UI program
@@ -23,8 +25,8 @@ UI's responsibilities
 1. Write the word run to prng-service.txt
 2. Read a number from that file
 3. Write that number to a file called 'image-service.txt'
-4. Read from that file a path to an image
-5. Output the image path to console.
+TODO: 4. Read from that file a path to an image
+TODO 5. Output the image path to console.
 
 */
 
@@ -35,18 +37,43 @@ func main() {
 
 	//prng communication
 	data := []byte("run")
+	// Might need to change file mode to 0666
+	// Should be fine since all programs will be running as
+	// my user
 	err := os.WriteFile(prngFilePath, data, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	prngNumber, err := os.ReadFile(prngFilePath)
+	// should not proceed if contents are not a number
+	prngFileData, err := os.ReadFile(prngFilePath)
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 	}
 
+	prngFileString := string(prngFileData)
+	prngNumber, err := strconv.ParseInt(prngFileString, 10, 64)
+	log.Print("Failed to parse prng number")
+	for err != nil {
+		log.Print("Waiting for prng number to be parseable.")
+		// Chill out
+		time.Sleep(5 * time.Second)
+		prngFileData, err = os.ReadFile(prngFilePath)
+		if err != nil {
+			log.Printf("Error reading file: %v", err)
+		}
+
+		prngFileString = string(prngFileData)
+		log.Printf("Prng file contents: %s", prngFileString)
+		prngNumber, err = strconv.ParseInt(prngFileString, 10, 64)
+		if err != nil {
+			log.Printf("Error parsing prng number: %v", err)
+		}
+	}
+
 	//image service communication
 	//data := []byte("run")
-	err = os.WriteFile(imageFilePath, prngNumber, 0644)
+	buffer := make([]byte, prngNumber)
+	err = os.WriteFile(imageFilePath, buffer, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
